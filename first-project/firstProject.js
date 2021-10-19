9806edb998259fb53f78efb72d58d43573e32d26
Jonas De Boeck <jonasdeboeck0@gmail.com>
2021-10-19 11:57:58 +0200

    add player movement

--- first-project/firstProject.js
+++ first-project/firstProject.js
@@ -1,7 +1,9 @@
 "use strict";
 
-let start = document.getElementById("start");
-let block = document.getElementById("block");
+const start = document.getElementById("start");
+const block = document.getElementById("block");
+const grid = document.getElementById("gamegrid");
+const width = 28;
 let position;
 
 function colorChange() {
@@ -52,4 +54,112 @@ document.body.onkeydown = function (direction) {
 };
 position = block.getBoundingClientRect();
 
+<<<<<<< Updated upstream
 // 318.3999938964844 668.5499877929688 338.3999938964844 648.5499877929688
+=======
+function removeRight() {
+  block.classList.remove("moveRight");
+}
+function removeLeft() {
+  block.classList.remove("moveLeft");
+}
+function removeUp() {
+  block.classList.remove("moveUp");
+}
+function removeDown() {
+  block.classList.remove("moveDown");
+}
+
+//gameboard//
+////////////
+const layout = [
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+  1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0,
+  1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1,
+  1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1,
+  1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1,
+  1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1,
+  1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0,
+  0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0,
+  1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1,
+  0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0,
+  1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0,
+  1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1,
+  1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1,
+  1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1,
+  1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0,
+  1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1,
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0,
+  1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1,
+  1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0,
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1,
+  1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0,
+  0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1,
+  1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0,
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+  1, 1, 1, 1,
+];
+const squares = [];
+function createBoard() {
+  for (let i = 0; i < layout.length; i++) {
+    const square = document.createElement("div");
+    grid.appendChild(square);
+    squares.push(square);
+
+    if (layout[i] === 1) {
+      squares[i].classList.add("wall");
+    }
+  }
+}
+createBoard();
+
+//player//
+///////////////////////////
+
+//starting position//
+let playerCurrentIndex = 754;
+squares[playerCurrentIndex].classList.add("player");
+
+//move//
+function movePlayer(e) {
+  squares[playerCurrentIndex].classList.remove("player");
+  switch (e.keyCode) {
+    case 37:
+      if (
+        playerCurrentIndex % width !== 0 &&
+        !squares[playerCurrentIndex - 1].classList.contains("wall")
+      )
+        playerCurrentIndex -= 1;
+      break;
+    case 38:
+      if (
+        playerCurrentIndex - width >= 0 &&
+        !squares[playerCurrentIndex - width].classList.contains("wall")
+      )
+        playerCurrentIndex -= width;
+      break;
+    case 39:
+      if (
+        playerCurrentIndex % width < width - 1 &&
+        !squares[playerCurrentIndex + 1].classList.contains("wall")
+      )
+        playerCurrentIndex += 1;
+      break;
+    case 40:
+      if (
+        playerCurrentIndex + width < width * width &&
+        !squares[playerCurrentIndex + width].classList.contains("wall")
+      )
+        playerCurrentIndex += width;
+      break;
+  }
+  squares[playerCurrentIndex].classList.add("player");
+}
+document.addEventListener("keyup", movePlayer);
+>>>>>>> Stashed changes
